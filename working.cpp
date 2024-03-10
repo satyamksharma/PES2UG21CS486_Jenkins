ad022e095f4d06aa8237d9c90cc228587e53a24f
Satyam Kumar <ksatyam433@gmail.com>
2024-03-10 13:12:27 +0530

    added working.cpp file

---
+++ working.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main() {
+    std::cout <<"1"<< std::endl;
+    std::cout <<"2"<< std::endl;
+    std::cout <<"3"<< std::endl;
+    std::cout <<"4"<< std::endl;
+    std::cout <<"5"<< std::endl;
+
+    return 0;
+}
